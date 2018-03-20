bd5e79959bfccf5466dd4bbe39eaa99a95af5d7e
bharanidharan81 <bharanidharan81@gmail.com>
2018-03-20 04:09:07 -0700

    Download Functionality Added

---
+++ FSA/Scripts/FSA.js
@@ -0,0 +1,71 @@
+﻿$(function ()
+{
+    var FSA = {};      
+
+    FSA.ViewModel = {
+        onDownloadClick: function ()
+        {     
+            //var $preparingFileModal = $("#preparing-file-modal");
+            //$preparingFileModal.dialog({ modal: true });
+            //$.fileDownload('http://localhost:8020/AppData/Download.zip', {
+            //    successCallback: function (url)
+            //    {
+            //        $preparingFileModal.dialog('close');
+            //    },
+            //    failCallback: function (responseHtml, url)
+            //    {
+
+            //        $preparingFileModal.dialog('close');
+            //        $("#error-modal").dialog({ modal: true });
+            //    }
+            //});            
+            window.downloadFile('http://localhost:8020/AppData/Download.zip');
+            return true;       
+        }
+    };       
+    ko.applyBindings(FSA.ViewModel);     
+});
+
+function downloadFile(url)
+{
+    var link = document.createElement("a");  
+    $(link).click(function (e)
+    {
+        e.preventDefault();
+        window.open(url, 'popupwindow', 'scrollbars,resizable');       
+    });
+    $(link).click();    
+}
+
+window.downloadFile = function (sUrl)
+{
+    if (window.downloadFile.isChrome || window.downloadFile.isSafari)
+    {
+        var link = document.createElement('a');
+        link.href = sUrl;
+        if (link.download !== undefined)
+        {
+            var fileName = sUrl.substring(sUrl.lastIndexOf('/') + 1, sUrl.length);
+            link.download = fileName;
+        }
+        if (document.createEvent)
+        {
+            var e = document.createEvent('MouseEvents');
+            e.initEvent('click', true, true);
+            link.dispatchEvent(e);
+            return true;
+        }
+    }
+    var query = '?download';
+    window.open(sUrl + query, '_self');
+};
+
+window.downloadFile.isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1; window.downloadFile.isSafari = navigator.userAgent.toLowerCase().indexOf('safari') > -1;
+
+//$(document).ready(function ()
+//{
+//    $('.download').click(function ()
+//    {
+//        window.downloadFile($(this).attr('data-download'));
+//    });
+//})
